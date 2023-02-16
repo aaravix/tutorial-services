2bd8af19905f74e2ceb7769a78840fc90d8a7924
Sanjit <team.aaravix@gmail.com>
2023-02-16 13:36:56 +0530

    initial

---
+++ Spring-Boot-Push-Notifications/src/main/java/com/example/demo/controller/PushNotificationController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+import org.springframework.http.HttpStatus;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.PushNotificationRequest;
+import com.example.demo.model.PushNotificationResponse;
+import com.example.demo.service.PushNotificationService;
+@RestController
+public class PushNotificationController {
+	
+	
+    private PushNotificationService pushNotificationService;
+    
+    public PushNotificationController(PushNotificationService pushNotificationService) {
+        this.pushNotificationService = pushNotificationService;
+    }
+    
+    @PostMapping("/notification/token")
+    public ResponseEntity sendTokenNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationToToken(request);
+        System.out.println("princr");
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+    
+}
+
+//https://www.youtube.com/watch?v=xcN5DB-LX3M
